731b9bcfd1ed90a910b9453f3c2d35dea5abc84b
shakil <mdshakilhossain720@gmail.com>
2025-02-01 19:18:26 +0600

    dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ProviderScope(
+    return const ProviderScope(
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         home: CounterScreen(),
---
+++ lib/views/cod/languafe.dart
@@ -0,0 +1,17 @@
+void main() {
+  int age = 30;
+  double height = 5.9;
+  String name = "shakil";
+  bool isActive = true;
+  List<String> hobbies = ["Reading", "Coding", "bangaldesh"];
+  Set<int> uniqueNumbers = {1, 2, 3};
+  Map<String, int> scores = {"Math": 90, "Science": 85};
+  dynamic anything = "Hello";
+
+  print("Name: $name, Age: $age, Height: $height");
+  print("Is Active: $isActive");
+  print("Hobbies: $hobbies");
+  print("Unique Numbers: $uniqueNumbers");
+  print("Scores: $scores");
+  print("Dynamic Value: $anything");
+}
